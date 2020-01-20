a8ee5481891d9fab995e4bb756b9603dc333ec5e
L W <lw@lw.is>
2020-01-20 19:42:46 +0100

    fix merge error

--- pipeline/workflow/qualitycheck.py
+++ pipeline/workflow/qualitycheck.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
 import os
 import json
 import pandas as pd
-
 
 from nipype.interfaces.base import ( 
     isdefined,
