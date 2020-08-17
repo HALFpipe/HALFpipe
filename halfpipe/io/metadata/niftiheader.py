Fix slice timing calculation

halfpipe/io/metadata/niftiheader.py
@@ -90,28 +90,33 @@ class NiftiheaderMetadataLoader:
 
         if key == "slice_timing":
             try:
-                slice_duration = header.get_slice_duration()
-                if np.isclose(slice_duration, 0.0):
-                    if self.fill(fileobj, "repetition_time"):
-                        if self.fill(fileobj, "slice_encoding_direction"):
-                            slice_dim = ["i", "j", "k"].index(
-                                fileobj.metadata.get("slice_encoding_direction")[0]
-                            )
-                            n_slices = header.get_data_shape()[slice_dim]
-                            slice_duration = fileobj.metadata.get("repetition_time") / n_slices
-                            header.set_slice_duration(slice_duration)
+                n_slices = None
+                if self.fill(fileobj, "slice_encoding_direction"):
+                    slice_dim = ["i", "j", "k"].index(
+                        fileobj.metadata.get("slice_encoding_direction")[0]
+                    )
+                    n_slices = header.get_data_shape()[slice_dim]
+                repetition_time = None
+                if self.fill(fileobj, "repetition_time"):
+                    repetition_time = fileobj.metadata.get("repetition_time") * 1000
+                nifti_slice_duration = header.get_slice_duration()
+                if n_slices is not None and repetition_time is not None:
+                    slice_duration = repetition_time / n_slices
+                    if nifti_slice_duration * n_slices < repetition_time - 2 * slice_duration:  # fudge factor
+                        logging.getLogger("halfpipe").warning(
+                            f'Unexpected nifti slice_duration = {nifti_slice_duration:f} for file "{fileobj.path}"\n'
+                            f"nifti slice_duration * n_slices ({nifti_slice_duration*n_slices:f} ms) "
+                            f"is much smaller than repetition_time ({repetition_time:f} ms), "
+                            "even though we expect them to be approximately equal\n"
+                            f"Setting slice_duration to repetition_time / n_slices = {slice_duration:f} ms"
+                        )
+                        header.set_slice_duration(slice_duration)
+                    if np.isclose(nifti_slice_duration, 0.0):
+                        header.set_slice_duration(slice_duration)
                 slice_times = header.get_slice_times()
                 slice_times = [s / 1000.0 for s in slice_times]  # need to be in seconds
                 if not np.allclose(slice_times, 0.0):
                     value = slice_times
-                    if self.fill(fileobj, "repetition_time"):
-                        max_slice_time = max(value)
-                        tr = fileobj.metadata.get("repetition_time")
-                        if not np.allclose(max_slice_time, tr):
-                            logging.getLogger("halfpipe").warning(
-                                f"Maximum slice time {max_slice_time:f} does not match repetition time {tr:f}\n"
-                                f'Image file "{fileobj.path}" has a slice_duration of {slice_duration:f} ms'
-                            )
             except nib.spatialimages.HeaderDataError:
                 return False
 