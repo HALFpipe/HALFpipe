ded2349046d54700e12dee9763a1ab9090b3796b
Lea Waller <lea.waller@charite.de>
2020-12-02 13:24:04 +0100

    Make test_flame1 run faster

--- halfpipe/interface/fslnumpy/tests/test_flame1.py
+++ halfpipe/interface/fslnumpy/tests/test_flame1.py
@@ -8,6 +8,7 @@ import pytest
 
 import os
 import tarfile
+from pathlib import Path
 
 import nibabel as nib
 import numpy as np
@@ -18,8 +19,9 @@ from ....resource import get as getresource
 from ..flame1 import FLAME1
 from ...fixes import FLAMEO as FSLFLAMEO
 
-from nipype.interfaces import fsl
+from nipype.interfaces import fsl, ants
 from nipype.pipeline import engine as pe
+from templateflow.api import get as get_template
 
 from ...imagemaths.merge import _merge, _merge_mask
 from ...stats.model import _group_model
@@ -57,18 +59,66 @@ def wakemandg_hensonrn(tmp_path_factory):
     return data
 
 
-@pytest.mark.timeout(7200)
+@pytest.fixture(scope="module")
+def mni_downsampled(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp(basename="mni_downsampled")
+
+    os.chdir(str(tmp_path))
+
+    tpl = get_template("MNI152NLin2009cAsym", resolution=2, desc="brain", suffix="mask")
+
+    result = ants.ResampleImageBySpacing(
+        dimension=3,
+        input_image=tpl,
+        out_spacing=(6, 6, 6)
+    ).run()
+
+    return result.outputs.output_image
+
+
+@pytest.fixture(scope="module")
+def wakemandg_hensonrn_downsampled(tmp_path_factory, wakemandg_hensonrn, mni_downsampled):
+    tmp_path = tmp_path_factory.mktemp(basename="wakemandg_hensonrn_downsampled")
+
+    os.chdir(str(tmp_path))
+
+    data = dict()
+
+    def _downsample(in_file):
+        result = ants.ApplyTransforms(
+            dimension=3,
+            input_image_type=0,
+            input_image=in_file,
+            reference_image=mni_downsampled,
+            interpolation="NearestNeighbor",
+            transforms=["identity"]
+        ).run()
+
+        return result.outputs.output_image
+
+    for k, v in wakemandg_hensonrn.items():
+        if isinstance(v, list):
+            data[k] = [_downsample(f) if Path(f).exists() else f for f in v]
+        else:
+            data[k] = v
+
+    return data
+
+
+@pytest.mark.timeout(600)
 @pytest.mark.parametrize("use_var_cope", [False, True])
-def test_FLAME1(tmp_path, wakemandg_hensonrn, use_var_cope):
+def test_FLAME1(tmp_path, wakemandg_hensonrn_downsampled, use_var_cope):
     os.chdir(str(tmp_path))
 
     # prepare
-    cope_files = wakemandg_hensonrn["stat-effect_statmap"]
-    var_cope_files = wakemandg_hensonrn["stat-variance_statmap"]
-    mask_files = wakemandg_hensonrn["mask"]
+    data = wakemandg_hensonrn_downsampled
 
-    subjects = wakemandg_hensonrn["subjects"]
-    spreadsheet_file = wakemandg_hensonrn["spreadsheet"]
+    cope_files = data["stat-effect_statmap"]
+    var_cope_files = data["stat-variance_statmap"]
+    mask_files = data["mask"]
+
+    subjects = data["subjects"]
+    spreadsheet_file = data["spreadsheet"]
 
     regressors, contrasts, _ = _group_model(
         subjects=subjects,
