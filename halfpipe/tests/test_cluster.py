39d1984a59eb81cd56a4f0567025531ebdb8ff52
Lea Waller <lea.waller@charite.de>
2021-04-17 09:59:09 +0200

    Use monkeypatch fixture

--- halfpipe/tests/test_cluster.py
+++ halfpipe/tests/test_cluster.py
@@ -2,7 +2,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-import os
 from pathlib import Path
 from collections import namedtuple, OrderedDict
 
@@ -10,7 +9,7 @@ from ..cli.parser import _build_parser
 from ..cluster import create_example_script
 
 
-def test_create_example_script(tmp_path: Path):
+def test_create_example_script(monkeypatch, tmp_path: Path):
     NodePlaceholder = namedtuple("NodePlaceholder", ["mem_gb"])
     WorkflowPlaceholder = namedtuple("WorkflowPlaceholder", ["uuid", "nodes"])
 
@@ -24,15 +23,8 @@ def test_create_example_script(tmp_path: Path):
         ("model", WorkflowPlaceholder(uuid="aaa", nodes=[node])),
     ])
 
-    previous_singularity_container = os.environ.get("SINGULARITY_CONTAINER")
-    os.environ["SINGULARITY_CONTAINER"] = "halfpipe.sif"
-
+    monkeypatch.setenv("SINGULARITY_CONTAINER", "halfpipe.sif")
     create_example_script(tmp_path, graphs, opts)
-
-    if previous_singularity_container is None:
-        del os.environ["SINGULARITY_CONTAINER"]
-    else:
-        os.environ["SINGULARITY_CONTAINER"] = previous_singularity_container
 
     assert Path(tmp_path / "submit.slurm.sh").is_file()
     assert Path(tmp_path / "submit.sge.sh").is_file()
