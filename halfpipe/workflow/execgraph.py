HALFpipe/HALFpipe

commit 97a43f60d4350b26116ef3aa574bd8d7a20012d4

Run generate_expanded_graph for each subgraph
- `generate_expanded_graph` runs a topological sort on the entire graph
  once for each iterable node, leading to quadratic scaling/complexity
- Calculation becomes infeasible around 4e5 nodes in a single graph
- Therefore, we now do the calculation separately for the subgraphs we
  derive

halfpipe/workflow/execgraph.py
@@ -11,10 +11,12 @@ import re
 from collections import OrderedDict
 from fnmatch import fnmatch
 from argparse import Namespace
+from copy import deepcopy
 
 import networkx as nx
 
 import nipype.pipeline.engine as pe
+from nipype.pipeline.engine.utils import merge_dict
 
 from .base import IdentifiableWorkflow
 from ..interface import LoadResult
@@ -58,13 +60,6 @@ def filter_subject_graphs(subject_graphs: OrderedDict, opts: Namespace) -> Order
     return subject_graphs
 
 
-class DontRunRunner:
-    plugin_args = dict()
-
-    def run(self, *args, **kwargs):
-        pass
-
-
 def extract_subject_name(hierarchy):
     m = re.fullmatch(r"single_subject_(?P<subjectname>.+)_wf", hierarchy[2])
     if m is not None:
@@ -101,30 +96,29 @@ def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) ->
     if graphs is None:
         logger.info(f'Initializing execution graph for workflow "{uuidstr}"')
 
-        execgraph = workflow.run(plugin=DontRunRunner())
-        execgraph.uuid = uuid
+        workflow._generate_flatgraph()
+        flatgraph = workflow._graph
 
-        logger.info("Splitting execution graph")
+        workflow._set_needed_outputs(flatgraph)
+
+        logger.info("Splitting graph")
 
         subject_nodes = dict()
-        for node in execgraph.nodes():
-
+        for node in flatgraph:
             hierarchy = node._hierarchy.split(".")
             assert len(hierarchy) >= 3
-
             subject_name = extract_subject_name(hierarchy)
-
             if subject_name is not None:
                 if subject_name not in subject_nodes:
                     subject_nodes[subject_name] = set()
                 subject_nodes[subject_name].add(node)
 
-        # make safe load
         all_subject_nodes = set.union(*subject_nodes.values())
-        model_nodes = set(execgraph.nodes()) - all_subject_nodes
+        model_nodes = set(flatgraph.nodes) - all_subject_nodes
 
+        # make safe load
         newnodes = dict()
-        for (v, u, c) in nx.edge_boundary(execgraph.reverse(), model_nodes, data=True):
+        for (v, u, c) in nx.edge_boundary(flatgraph.reverse(), model_nodes, data=True):
             u.keep = True  # don't allow results to be deleted
 
             newu = newnodes.get(u.fullname)
@@ -134,32 +128,37 @@ def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) ->
                 newu.config = u.config
                 newnodes[u.fullname] = newu
 
-            execgraph.add_edge(newu, v, attr_dict=c)
+            flatgraph.add_edge(newu, v, **c)
 
             newuresultfile = Path(newu.output_dir()) / f"result_{newu.name}.pklz"
             for outattr, inattr in c["connect"]:
                 newu.needed_outputs = [*newu.needed_outputs, outattr]
                 v.input_source[inattr] = (newuresultfile, outattr)
 
-        subject_graphs = OrderedDict(
-            sorted(
-                [
-                    (
-                        workflow.bids_to_sub_id_map.get(s, s),
-                        execgraph.subgraph(nodes).copy(),
-                    )
-                    for s, nodes in subject_nodes.items()
-                ],
-                key=lambda t: t[0],
-            )
-        )
-        execgraph.remove_nodes_from(all_subject_nodes)
-        model_graph = execgraph
+        logger.info("Expanding subgraphs")
 
-        graphs = OrderedDict([
-            *subject_graphs.items(),
-            ("model", model_graph),
-        ])
+        graphs = OrderedDict()
+
+        def add_graph(s, graph):
+            graph = pe.generate_expanded_graph(graph)
+
+            for index, node in enumerate(graph):
+                node.config = merge_dict(deepcopy(workflow.config), node.config)
+                node.base_dir = workflow.base_dir
+                node.index = index
+
+            workflow._configure_exec_nodes(graph)
+
+            graphs[s] = graph
+
+        for s, nodes in sorted(subject_nodes.items(), key=lambda t: t[0]):
+            s = workflow.bids_to_sub_id_map.get(s, s)
+
+            subgraph = flatgraph.subgraph(nodes).copy()
+            add_graph(s, subgraph)
+
+        flatgraph.remove_nodes_from(all_subject_nodes)
+        add_graph("model", flatgraph)
 
         for graph in graphs.values():
             graph.uuid = uuid

commit 38ca3a98e4d0b23cb7169227d63becba2fd586c8

Fix `init_execgraph` to not depend on `LoadResult` nodes
- Improve performance
- Fix loading the correct result files from other chunks which was
  broken in 97a43f60d4350b26116ef3aa574bd8d7a20012d4

halfpipe/workflow/execgraph.py
@@ -2,13 +2,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-from typing import Union
+from typing import Union, Dict
 
 import logging
 from pathlib import Path
 from shutil import copyfile
 import re
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from fnmatch import fnmatch
 from argparse import Namespace
 from copy import deepcopy
@@ -16,11 +16,12 @@ from copy import deepcopy
 import networkx as nx
 
 import nipype.pipeline.engine as pe
+from nipype.interfaces import utility as niu
 from nipype.pipeline.engine.utils import merge_dict
 
+from ..fixes import Node
 from .base import IdentifiableWorkflow
-from ..interface import LoadResult
-from ..utils import b32digest, resolve
+from ..utils import resolve
 from ..io import DictListFile, cacheobj, uncacheobj
 from ..resource import get as getresource
 from .constants import constants
@@ -66,6 +67,57 @@ def extract_subject_name(hierarchy):
         return m.group("subjectname")
 
 
+def find_input_source(graph: nx.DiGraph, u: Node, v: Node, c: Dict):
+    stack = [
+        (u, v, source_info, field) for source_info, field in c["connect"]
+    ]
+
+    result = list()
+
+    while len(stack) > 0:
+        node, v, source_info, field = stack.pop(0)
+        if not isinstance(node.interface, niu.IdentityInterface):
+            result.append((node, v, source_info, field))
+            continue
+        assert not isinstance(source_info, tuple)
+        for u, _, k in graph.in_edges(node, data=True):
+            for u_source_info, node_field in k["connect"]:
+                if source_info == node_field:
+                    stack.append(
+                        (u, v, u_source_info, field)
+                    )
+
+    stack = result
+    result = list()
+
+    while len(stack) > 0:
+        u, node, source_info, field = stack.pop(0)
+        if not isinstance(node.interface, niu.IdentityInterface):
+            result.append((u, node, source_info, field))
+            continue
+        for _, v, k in graph.out_edges(node, data=True):
+            for node_source_info, v_field in k["connect"]:
+                if node_source_info == field:
+                    stack.append(
+                        (u, v, source_info, v_field)
+                    )
+
+    input_source_dict = defaultdict(dict)
+    for u, v, source_info, field in result:
+        u.keep = True  # don't allow results to be deleted
+
+        u._output_dir = None  # reset this just in case
+
+        output_dir = u.output_dir()
+        assert isinstance(output_dir, str)
+
+        result_file = Path(output_dir) / f"result_{u.name}.pklz"
+
+        input_source_dict[v][field] = (result_file, source_info)
+
+    return input_source_dict
+
+
 def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) -> OrderedDict:
     logger = logging.getLogger("halfpipe")
 
@@ -105,6 +157,8 @@ def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) ->
 
         subject_nodes = dict()
         for node in flatgraph:
+            node.base_dir = workflow.base_dir  # make sure to use correct base path
+
             hierarchy = node._hierarchy.split(".")
             assert len(hierarchy) >= 3
             subject_name = extract_subject_name(hierarchy)
@@ -116,29 +170,12 @@ def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) ->
         all_subject_nodes = set.union(*subject_nodes.values())
         model_nodes = set(flatgraph.nodes) - all_subject_nodes
 
-        # make safe load
-        newnodes = dict()
+        input_source_dict = defaultdict(dict)
         for (v, u, c) in nx.edge_boundary(flatgraph.reverse(), model_nodes, data=True):
-            u.keep = True  # don't allow results to be deleted
+            edge_input_source_dict = find_input_source(flatgraph, u, v, c)
 
-            u.base_dir = workflow.base_dir  # make sure to use correct base path
-            u._output_dir = None  # reset this just in case
-
-            newu = newnodes.get(u.fullname)
-            if newu is None:
-                udigest = b32digest(u.fullname)[:8]
-                newu = pe.Node(LoadResult(u), name=f"load_{udigest}", base_dir=modeldir)
-                newu.config = u.config
-                newnodes[u.fullname] = newu
-
-            flatgraph.add_edge(newu, v, **c)
-
-            output_dir = newu.output_dir()
-            assert isinstance(output_dir, str)
-            newuresultfile = Path(output_dir) / f"result_{newu.name}.pklz"
-            for outattr, inattr in c["connect"]:
-                newu.needed_outputs = [*newu.needed_outputs, outattr]
-                v.input_source[inattr] = (newuresultfile, outattr)
+            for v, input_sources in edge_input_source_dict.items():
+                input_source_dict[v].update(input_sources)
 
         logger.info("Expanding subgraphs")
 
@@ -153,6 +190,10 @@ def init_execgraph(workdir: Union[Path, str], workflow: IdentifiableWorkflow) ->
                 node.index = index
 
             workflow._configure_exec_nodes(graph)
+
+            for node in graph:
+                if node in input_source_dict:
+                    node.input_source.update(input_source_dict[node])
 
             graphs[s] = graph
 