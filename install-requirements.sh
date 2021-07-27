c945789a53de4630898f65828a19afefdc599b22
Lea Waller <lea.waller@charite.de>
2021-07-27 12:10:45 +0200

    Use `conda-forge`
    - Slower build
    - Better control over Python environment
    - Make sure that system dependencies are installed

--- install-requirements.sh
+++ install-requirements.sh
@@ -74,6 +74,11 @@ run_cmd pip install --upgrade pip
 
 printf '%s\n' --------------------
 
+run_cmd conda config --add channels conda-forge
+run_cmd conda config --set channel_priority strict
+
+printf '%s\n' --------------------
+
 CONDA_PACKAGES=()
 PIP_PACKAGES=()
 
