0cc3c0e0aedae8226b5e78dc686881a46f6bc95e
Lea Waller <lea.waller@charite.de>
2021-07-29 09:51:24 +0200

    Mark workflow constants as `Final`
    - Prevent runtime changes leading to errors

--- halfpipe/workflow/constants.py
+++ halfpipe/workflow/constants.py
@@ -2,9 +2,11 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+from typing_extensions import Final
+
 
 class constants:
-    reference_space = "MNI152NLin2009cAsym"
-    reference_res = 2
+    reference_space: Final[str] = "MNI152NLin2009cAsym"
+    reference_res: Final[int] = 2
 
-    workflowdir = "nipype"
+    workflowdir: Final[str] = "nipype"
