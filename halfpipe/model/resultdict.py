b3627a1f6244c824cd708076f598f660f4e47bae
Lea Waller <lea.waller@charite.de>
2021-08-11 09:04:09 +0200

    Simplify `ResultdictSchema`
    - Disable most validation
    - To be re-introduced in a later version

--- halfpipe/model/resultdict.py
+++ halfpipe/model/resultdict.py
@@ -6,56 +6,9 @@
 
 """
 
-from pathlib import Path
-
-from marshmallow import Schema, RAISE, fields, ValidationError
+from marshmallow import Schema, RAISE, fields
 
 from .tags import ResultdictTagsSchema
-from ..stats.algorithms import algorithms, modelfit_aliases
-
-
-def validate_file(v):
-    def is_ok(x):
-        return isinstance(x, str) and Path(x).is_file()
-
-    if is_ok(v):
-        return
-    if isinstance(v, (tuple, list)) and all(is_ok(x) for x in v):
-        return
-    raise ValidationError("Need to be either a file or a list of files")
-
-
-image_types = frozenset([
-    # according to https://fmriprep.org/en/stable/outputs.html
-    "bold",
-    "mask",
-
-    *modelfit_aliases.values(),
-
-    # according to https://github.com/bids-standard/bids-specification/blob/derivatives/src/05-derivatives/05-functional-derivatives.md
-    "tsnr",
-    "alff",
-    "falff",
-    "reho",
-    "timeseries",
-
-    #
-    "matrix",
-    "regressors",
-
-    # stats outputs
-    *(
-        output
-        for algorithm in algorithms.values()
-        for outputs in [algorithm.model_outputs, algorithm.contrast_outputs]
-        for output in outputs
-    ),
-])
-
-ResultdictImagesSchema = Schema.from_dict({
-    image_type: fields.Raw(validate=validate_file)
-    for image_type in image_types
-})
 
 
 class ResultdictSchema(Schema):
@@ -64,8 +17,6 @@ class ResultdictSchema(Schema):
 
     tags = fields.Nested(ResultdictTagsSchema(), dump_default=dict())
     metadata = fields.Dict(keys=fields.Str(), values=fields.Raw(), dump_default=dict())
-    images = fields.Nested(ResultdictImagesSchema(), dump_default=dict())
-    reports = fields.Dict(
-        keys=fields.Str(), values=fields.Raw(validate=validate_file), dump_default=dict()
-    )
+    images = fields.Dict(keys=fields.Str(), values=fields.Raw(), dump_default=dict())
+    reports = fields.Dict(keys=fields.Str(), dump_default=dict())
     vals = fields.Dict(keys=fields.Str(), values=fields.Raw(), dump_default=dict())
