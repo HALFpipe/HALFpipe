4ea145ead59dafa5a9eb82daf916e093ac1dc3fb
Lea Waller <lea.waller@charite.de>
2022-01-25 12:54:53 +0100

    Compatibility with `frozendict==2.2.0`

--- halfpipe/interface/resultdict/tests/test_aggregate.py
+++ halfpipe/interface/resultdict/tests/test_aggregate.py
@@ -2,7 +2,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-from typing import Sequence
+from typing import Sequence, Mapping
 
 from math import isclose
 import numpy as np
@@ -41,21 +41,21 @@ def test_aggregate_any_dict():
     for c in counts:
         count = Count.Schema().load(c)
         assert isinstance(count, Count)
-        assert isinstance(count.value, dict)
+        assert isinstance(count.value, Mapping)
 
     counts = aggregate_any([counts, counts])
     assert isinstance(counts, Sequence)
     for c in counts:
         count = Count.Schema().load(c)
         assert isinstance(count, Count)
-        assert isinstance(count.value, dict)
+        assert isinstance(count.value, Mapping)
 
     counts = aggregate_any([tuple(counts), tuple(counts)])
     assert isinstance(counts, Sequence)
     for c in counts:
         count = Count.Schema().load(c)
         assert isinstance(count, Count)
-        assert isinstance(count.value, dict)
+        assert isinstance(count.value, Mapping)
 
 
 def test_aggregate_resultdicts():
