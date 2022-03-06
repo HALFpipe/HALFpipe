49373e65aa0be56b048a6c216f2c4a7366da1c8b
Lea Waller <lea.waller@charite.de>
2022-03-06 12:58:06 +0100

    Fix abstract `Distribution` class
    - Use instances instead of static methods
    - Fix mypy errors

--- halfpipe/stats/miscmaths.py
+++ halfpipe/stats/miscmaths.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""
-"""
-
-from typing import Type, Union
-from abc import ABC, abstractclassmethod
 
 import math
+from abc import ABC, abstractmethod
 
-from mpmath import mp, mpf, autoprec, mpmathify
+from mpmath import autoprec, mp, mpf, mpmathify
 
 
 def erfinv(a: mpf, tol: float = 1e-16) -> mpf:
@@ -31,18 +27,81 @@ def normppf(p: mpf) -> mpf:
 
 
 class Distribution(ABC):
-    @abstractclassmethod
-    def cdf(cls, *args: mpf) -> mpf:
+    @abstractmethod
+    def cdf(self, x: mpf) -> mpf:
         raise NotImplementedError
 
-    @classmethod
-    def cdfc(cls, *args: mpf) -> mpf:
-        return mpf("1") - cls.cdf(*args)
+    def cdfc(self, x: mpf) -> mpf:
+        return mpf("1") - self.cdf(x)
+
+    def auto_convert(self, x: float, max_prec: int = 2 ** 13) -> float:
+        if math.isnan(x):
+            return math.nan  # skip computation
+
+        elif math.isinf(x):
+            return x
+
+        prec = mp.prec
+
+        cdf = self.cdf
+        p = cdf(mpmathify(x))
+
+        use_complement = mp.almosteq(p, mpf("1"))
+        # it required many more bits to represent a number close to one than a
+        # number close to zero
+        if use_complement:
+            cdf = self.cdfc
+
+        z = math.nan
+
+        try:
+            # infer base precision
+
+            while mp.prec < max_prec:
+                p = cdf(mpmathify(x))
+
+                if not mp.almosteq(p, mpf("1")) and not mp.almosteq(p, mpf("0")):
+
+                    # we have sufficient precision to represent the p-value
+
+                    calc_z = autoprec(lambda: normppf(cdf(mpmathify(x))))
+                    z = float(calc_z())
+
+                    break
+
+                if mp.prec <= 2 ** 12:
+                    mp.prec += 2 ** 8
+                else:
+                    mp.prec += 2 ** 12
+
+            # skip calculation
+
+            mp.prec = prec  # reset precision so that almosteq cannot fail
+
+            if not math.isfinite(z):
+                if mp.almosteq(p, mpf("1")):
+                    z = math.inf
+                else:
+                    assert mp.almosteq(p, mpf("0"))
+                    z = -math.inf
+
+            if use_complement:
+                z = -z
+
+            return z
+        finally:
+            mp.prec = prec
 
 
 class TDistribution(Distribution):
-    @classmethod
-    def cdf(cls, x: mpf, nu: mpf) -> mpf:
+    def __init__(self, nu: int) -> None:
+        super().__init__()
+
+        self.nu: int = nu
+
+    def cdf(self, x: mpf) -> mpf:
+        nu = mpmathify(self.nu)
+
         x2 = x * x
         z = nu / (nu + x2)
 
@@ -59,14 +118,21 @@ class TDistribution(Distribution):
         else:
             return p
 
-    @classmethod
-    def cdfc(cls, x: mpf, nu: mpf) -> mpf:
-        return cls.cdf(-x, nu)
+    def cdfc(self, x: mpf) -> mpf:
+        return self.cdf(-x)
 
 
 class FDistribution(Distribution):
-    @staticmethod
-    def _integral(x: mpf, d1: mpf, d2: mpf, inv: bool):
+    def __init__(self, d1: int, d2: int) -> None:
+        super().__init__()
+
+        self.d1: int = d1
+        self.d2: int = d2
+
+    def _integral(self, x: mpf, inv: bool):
+        d1 = mpmathify(self.d1)
+        d2 = mpmathify(self.d2)
+
         z = d1 * x / (d1 * x + d2)
 
         if inv is False:
@@ -81,90 +147,30 @@ class FDistribution(Distribution):
             regularized=True,
         )
 
-    @classmethod
-    def cdf(cls, x: mpf, d1: mpf, d2: mpf) -> mpf:
-        return cls._integral(x, d1, d2, inv=False)
+    def cdf(self, x: mpf) -> mpf:
+        return self._integral(x, inv=False)
 
-    @classmethod
-    def cdfc(cls, x: mpf, d1: mpf, d2: mpf) -> mpf:
-        return cls._integral(x, d1, d2, inv=True)
+    def cdfc(self, x: mpf) -> mpf:
+        return self._integral(x, inv=True)
 
 
 class ChisqDistribution(Distribution):
-    @classmethod
-    def cdf(cls, x: mpf, k: mpf) -> mpf:
+    def __init__(self, k: int) -> None:
+        super().__init__()
+
+        self.k: int = k
+
+    def cdf(self, x: mpf) -> mpf:
+        k = mpmathify(self.k)
         return mp.gammainc(k / mpf("2"), 0, x / mpf("2"), regularized=True)
 
-    @classmethod
-    def cdfc(cls, x: mpf, k: mpf) -> mpf:
+    def cdfc(self, x: mpf) -> mpf:
+        k = mpmathify(self.k)
         return mp.gammainc(k / mpf("2"), x / mpf("2"), mp.inf, regularized=True)
 
 
-def auto_convert(
-    d: Type[Distribution],
-    *args: Union[float, int],
-    max_prec: int = 2 ** 13
-) -> float:
-    if any(math.isnan(a) for a in args):
-        return math.nan  # skip computation
-    elif math.isinf(args[0]):  # first argument is always the statistic
-        return args[0]
-
-    prec = mp.prec
-
-    cdf = d.cdf
-    p = cdf(*map(mpmathify, args))
-
-    use_complement = mp.almosteq(p, mpf("1"))
-    # it required many more bits to represent a number close to one than a
-    # number close to zero
-    if use_complement:
-        cdf = d.cdfc
-
-    z = math.nan
-
-    try:
-        # infer base precision
-
-        while mp.prec < max_prec:
-            p = cdf(*map(mpmathify, args))
-
-            if not mp.almosteq(p, mpf("1")) and not mp.almosteq(p, mpf("0")):
-
-                # we have sufficient precision to represent the p-value
-
-                z = float(
-                    autoprec(lambda: normppf(cdf(*map(mpf, args))))()
-                )
-
-                break
-
-            if mp.prec <= 2 ** 12:
-                mp.prec += 2 ** 8
-            else:
-                mp.prec += 2 ** 12
-
-        # skip calculation
-
-        mp.prec = prec  # reset precision so that almosteq cannot fail
-
-        if not math.isfinite(z):
-            if mp.almosteq(p, mpf("1")):
-                z = math.inf
-            else:
-                assert mp.almosteq(p, mpf("0"))
-                z = -math.inf
-
-        if use_complement:
-            z = -z
-
-        return z
-    finally:
-        mp.prec = prec
-
-
 def t2z_convert(x: float, nu: int, **kwargs) -> float:
-    return auto_convert(TDistribution, x, nu, **kwargs)
+    return TDistribution(nu).auto_convert(x, **kwargs)
 
 
 def f2z_convert(x: float, d1: int, d2: int, **kwargs) -> float:
@@ -172,7 +178,7 @@ def f2z_convert(x: float, d1: int, d2: int, **kwargs) -> float:
     if x <= 0 or d1 <= 0 or d2 <= 0:
         return -math.inf
 
-    return auto_convert(FDistribution, x, d1, d2, **kwargs)
+    return FDistribution(d1, d2).auto_convert(x, **kwargs)
 
 
 def chisq2z_convert(x: float, k: int, **kwargs) -> float:
@@ -180,4 +186,4 @@ def chisq2z_convert(x: float, k: int, **kwargs) -> float:
     if x <= mpf("0") or k <= mpf("0"):
         return -math.inf
 
-    return auto_convert(ChisqDistribution, x, k, **kwargs)
+    return ChisqDistribution(k).auto_convert(x, **kwargs)
