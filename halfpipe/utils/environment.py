425c59ca9b5e00431864dec2ecb9527618ceabc9
Jason A. Staph <jstaph@staph.us>
2022-05-08 12:33:14 -0400

    chore(linting): more linting

--- halfpipe/utils/environment.py
+++ halfpipe/utils/environment.py
@@ -2,15 +2,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-import os
 import logging
-
+import os
 from argparse import Namespace
 from glob import glob
 from pathlib import Path
 
 from .path import resolve
-
 
 logger = logging.getLogger()
 
