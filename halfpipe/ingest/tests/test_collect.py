76086649812fceb255f645fca5020c3ace4a65da
domiii <dominik@domiiis-MacBook-Pro.local>
2022-07-18 14:13:22 +0200

    Tests still failing for all cases with expected count==0

--- halfpipe/ingest/tests/test_collect.py
+++ halfpipe/ingest/tests/test_collect.py
@@ -56,3 +56,165 @@ def test_collect_fieldmaps_epi(pe_dirs, expected_count):
 
     fieldmaps = collect_fieldmaps(database, bold_file_path)
     assert len(fieldmaps) == expected_count
+
+
+@pytest.mark.parametrize(
+    "case1_suffix, case1_count",
+    [
+        (["", "", "phasediff"], 0),
+        (["magnitude1", "", "phasediff"], 3),
+        (["magnitude1", "magnitude2", "phasediff"], 3),
+    ],
+)
+def test_collect_fieldmaps_phasediff(case1_suffix, case1_count):
+    database = Database(Spec(datetime.now, list()))
+
+    bold_file_path = "bold.nii.gz"
+
+    files = [
+        # Phase Difference Maps and atleast one magnitude image
+        File(
+            path="magnitude1.nii.gz",
+            datatype="fmap",
+            suffix=case1_suffix[0],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+            metadata=dict(echo_time_1=4.63),
+        ),
+        File(
+            path="magnitude2.nii.gz",
+            datatype="fmap",
+            suffix=case1_suffix[1],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+            metadata=dict(echo_time_2=7.09),
+        ),
+        File(
+            path="phasediff.nii.gz",
+            datatype="fmap",
+            suffix=case1_suffix[2],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+        File(
+            path=bold_file_path,
+            datatype="func",
+            suffix="bold",
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+    ]
+
+    for file in files:
+        database.put(file)
+
+    fieldmaps = collect_fieldmaps(database, bold_file_path)
+    assert len(fieldmaps) == case1_count
+
+
+@pytest.mark.parametrize(
+    "case2_suffix, case2_count",
+    [
+        (["", "", "", ""], 0),
+        (["", "", "phase1", ""], 0),
+        (["", "", "phase1", "phase2"], 2),
+        (["magnitude1", "", "phase1", "phase2"], 4),
+        (["", "magnitude2", "phase1", "phase2"], 4),
+        (["magnitude1", "magnitude2", "phase1", "phase2"], 4),
+    ],
+)
+def test_collect_fieldmaps_twophase(case2_suffix, case2_count):
+    database = Database(Spec(datetime.now, list()))
+
+    bold_file_path = "bold.nii.gz"
+
+    files = [
+        File(
+            path="magnitude1.nii.gz",
+            datatype="fmap",
+            suffix=case2_suffix[0],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+            metadata=dict(echo_time_1=None),
+        ),
+        File(
+            path="magnitude2.nii.gz",
+            datatype="fmap",
+            suffix=case2_suffix[1],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+            metadata=dict(echo_time_2=None),
+        ),
+        File(
+            path="phase1.nii.gz",
+            datatype="fmap",
+            suffix=case2_suffix[2],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+        File(
+            path="phase2.nii.gz",
+            datatype="fmap",
+            suffix=case2_suffix[3],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+        File(
+            path=bold_file_path,
+            datatype="func",
+            suffix="bold",
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+    ]
+
+    for file in files:
+        database.put(file)
+
+    fieldmaps = collect_fieldmaps(database, bold_file_path)
+    assert len(fieldmaps) == case2_count
+
+
+@pytest.mark.parametrize(
+    "case3_suffix, case3_count",
+    [
+        (["fieldmap", "magnitude"], 2),
+        (["fieldmap", ""], 0),
+        (["", "magnitude"], 0),
+    ],
+)
+def test_collect_fieldmaps_direct(case3_suffix, case3_count):
+    database = Database(Spec(datetime.now, list()))
+
+    bold_file_path = "bold.nii.gz"
+
+    files = [
+        File(
+            path="fieldmap.nii.gz",
+            datatype="fmap",
+            suffix=case3_suffix[0],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+            metadata=dict(units=("Hz", "rad/s", "T")),
+        ),
+        File(
+            path="magnitude.nii.gz",
+            datatype="fmap",
+            suffix=case3_suffix[1],
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+        File(
+            path=bold_file_path,
+            datatype="func",
+            suffix="bold",
+            extension=".nii.gz",
+            tags=dict(sub="01"),
+        ),
+    ]
+
+    for file in files:
+        database.put(file)
+
+    fieldmaps = collect_fieldmaps(database, bold_file_path)
+    assert len(fieldmaps) == case3_count
