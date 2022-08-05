08aa5545ed4f35e73f739ad30a77402d366d3799
Lea Waller <lea.waller@charite.de>
2022-08-05 08:31:23 +0200

    Add `FileIndex`, which will replace `Database` soon
    - Simpler, more modular architecture
    - Also support indexing BIDS derivatives

---
+++ halfpipe/file_index/tests/test_bids.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+from pathlib import Path
+
+from ..bids import parse
+
+
+def test_parse():
+    path = Path(
+        "qtab/reports/sub-0003/figures/sub-0003_task-emotionalconflict_epi_norm_rpt.svg"
+    )
+    tags = parse(path)
+
+    assert isinstance(tags, dict)
+    assert len(tags) == 4
+
+    assert tags["sub"] == "0003"
+    assert tags["task"] == "emotionalconflict"
+    assert tags["suffix"] == "epi_norm_rpt"
+    assert tags["extension"] == ".svg"
+
+    path = Path("qtab/derivatives/halfpipe/sub-0003/func")
+    tags = parse(path)
+
+    assert isinstance(tags, dict)
+    assert len(tags) == 1
+
+    assert tags["suffix"] == "func"
