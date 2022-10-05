9b1c19ca95c6704a660fb1e8ceec7cae2dd5044f
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-05 13:15:36 +0000

    Adding tarfile member sanitization to extractall()

--- halfpipe/stats/tests/conftest.py
+++ halfpipe/stats/tests/conftest.py
@@ -26,7 +26,26 @@ def wakemandg_hensonrn_raw(tmp_path_factory):
     inputtarpath = getresource("wakemandg_hensonrn_statmaps.tar.gz")
 
     with tarfile.open(inputtarpath) as fp:
-        fp.extractall(tmp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fp, tmp_path)
 
     subjects = [f"{i+1:02d}" for i in range(16)]
     suffixes = ["stat-effect_statmap", "stat-variance_statmap", "mask"]
--- halfpipe/workflows/tests/test_workflow.py
+++ halfpipe/workflows/tests/test_workflow.py
@@ -98,7 +98,26 @@ def atlas_harvard_oxford(tmp_path_factory):
     inputtarpath = get_resource("HarvardOxford.tgz")
 
     with tarfile.open(inputtarpath) as fp:
-        fp.extractall(tmp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fp, tmp_path)
 
     maps = {
         m: (
