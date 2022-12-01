7b6fe56e8fece95215601f3d6994f9b1a17e5373
dominikgoeller <22domig@gmail.com>
2022-12-01 13:08:39 +0100

    changed recursive walk

--- halfpipe/ingest/tests/test_resolve.py
+++ halfpipe/ingest/tests/test_resolve.py
@@ -54,12 +54,50 @@ def test__resolve_bids(tmp_path: Path, openneuroID: str):
 
     json_file = json.loads(r.text)
 
-    def recursive_walk(neuro_dict, file_list: list = []):
+    # def recursive_walk(neuro_dict, file_list: list = []):
+    #    base_list = neuro_dict["data"]["snapshot"]["files"]  # returns list of all files
+    #    for val in base_list:
+    #        if not val["directory"]:
+    #            file_list.append(val["filename"])
+    #        cur_id = val["id"]
+    #        query = f"""query{{
+    #            snapshot(datasetId: "{openneuroID}", tag: "1.0.0"){{
+    #                files(tree: "{cur_id}"){{
+    #                    id
+    #                    key
+    #                    filename
+    #                    directory
+    #                    }}
+    #                }}
+    #            }}"""
+    #        r = requests.post(gql_url, json={"query": query})
+    #        json_dict = json.loads(r.text)
+    #        recursive_walk(json_dict)
+    #    return file_list
+
+    def recursive_walk_wpath(neuro_dict, file_list=[], build_path=None):
         base_list = neuro_dict["data"]["snapshot"]["files"]  # returns list of all files
-        for val in base_list:
+        if build_path is None:
+            build_path = []
+        for (i, val) in enumerate(base_list):
             if not val["directory"]:
-                file_list.append(val["filename"])
+                p = "/".join(str(x) for x in build_path)
+                x = p + "/" + val["filename"]
+                if x.startswith("/"):
+                    x = x[1:]
+                file_list.append(x.strip())
+                continue
             cur_id = val["id"]
+            cur_fname = val["filename"]
+            if (
+                i == 0 and len(build_path) == 2
+            ):  # delete current sub-0* folder when going to next sub-0*
+                del build_path[0]
+            elif (
+                i > 0 and build_path
+            ):  # delete subfolders of sub-0* folders when going to next subfolder
+                del build_path[-1]
+            build_path.append(cur_fname)
             query = f"""query{{
                 snapshot(datasetId: "{openneuroID}", tag: "1.0.0"){{
                     files(tree: "{cur_id}"){{
@@ -72,10 +110,10 @@ def test__resolve_bids(tmp_path: Path, openneuroID: str):
                 }}"""
             r = requests.post(gql_url, json={"query": query})
             json_dict = json.loads(r.text)
-            recursive_walk(json_dict)
+            recursive_walk_wpath(json_dict, build_path=build_path)
         return file_list
 
-    file_list: list[str] = recursive_walk(neuro_dict=json_file)
+    file_list: list[str] = recursive_walk_wpath(neuro_dict=json_file)
 
     for line in file_list:
         if not isinstance(line, str):
