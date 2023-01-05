7b186b13d5276348dea428f0d15ca4b75cb64d49
dominikgoeller <22domig@gmail.com>
2023-01-05 12:37:55 +0100

    test_parsedescrip with 7 test cases without units specified and deleted unused imports/variables

--- halfpipe/ingest/metadata/tests/test_parsedescrip.py
+++ halfpipe/ingest/metadata/tests/test_parsedescrip.py
@@ -1,42 +1,38 @@
 # -*- coding: utf-8 -*-
-import subprocess
 
 import nibabel as nib
-import numpy as np
 import pytest
 
 from ..niftiheader import parse_descrip  # noqa
 
 
-@pytest.mark.parametrize()
-def dump_header(img_fname, out_fname):
-    """Use fslhd to dump an image header to a text file
-    Parameters
-    ----------
-    img_fname : str
-        Filename of the image to use.
-    out_fname : str
-        Filename to save the header to.
-    """
-    with open(out_fname, "w") as fid:
-        subprocess.check_call(["fslhd", "-x", img_fname], stdout=fid)
+@pytest.mark.parametrize(
+    "test_descrip, expected_dict",
+    [
+        (
+            "te=0.3;tr=1",
+            {"echo_time": 0.3, "repetition_time": 1.0},
+        ),  # case1 both s; te < 1 & tr <= 100
+        (
+            "te=300;tr=200",
+            {"echo_time": 0.3, "repetition_time": 0.2},
+        ),  # case2 both ms; te >=1 & tr > 100
+        (
+            "te=0.3;tr=1000",
+            {"echo_time": 0.3, "repetition_time": 1.0},
+        ),  # case3 te/s tr/ms; te < 1 & tr > 100
+        (
+            "te=300;tr=1",
+            {"echo_time": 0.3, "repetition_time": 1.0},
+        ),  # case4 te/ms tr/s; te >=1 & tr <= 100
+        ("te=0.3", {"echo_time": 0.3}),  # case5 te/specified tr/none;
+        ("tr=1", {"repetition_time": 1.0}),  # case6 te/None tr/specified;
+        ("", {}),  # case7 both None;
+    ],
+)
+def test_parse_descrip(tmp_path, test_descrip, expected_dict):
+    header = nib.Nifti1Header()
+    header["descrip"] = test_descrip
 
-
-def test_NiftiheaderLoader_parse_descrip(tmp_path):
-    size = (2, 3, 4)
-
-    # out_fname = str(tmp_path / "text.txt")
-    fname = str(tmp_path / "testdata.nii")
-
-    nib.Nifti1Image(np.zeros(size), np.eye(4)).to_filename(fname)
-    img: nib.Nifti1Image = nib.load(fname)
-    # hdr: nib.Nifti1Header = img.header
-    img.header["descrip"] = "TE=30"  # do it like this
-
-    # hdr: nib.Nifti1Header = img.header
-    # image.header["descrip"]
-    # nib.save(fname)
-    # dump_header(img_fname=fname, out_fname=out_fname)
-
-    descrip_dict = parse_descrip(header=img.header)
-    print("TEST", descrip_dict.keys())
+    descrip_dict = parse_descrip(header=header)
+    assert descrip_dict == expected_dict
