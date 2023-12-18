417cd5060193c13a728b7b73ae93ceb41887d9a7
Vi <vi@Flamenco.local>
2023-12-18 10:45:32 +0100

    fix build recipe for rmath on macos

--- recipes/rmath/build.sh
+++ recipes/rmath/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e # Stop on any error
 export LIBnn=lib
 
+echo "Configuring..." # Configure with verbose output
 ./configure \
     --prefix="${PREFIX}" \
     --enable-lto=yes \
@@ -22,13 +24,26 @@ export LIBnn=lib
     --with-readline=no \
     --with-recommended-packages=no \
     --with-tcltk=no \
-    --with-x=no
+    --with-x=no || { echo "Configuration failed"; exit 1; }
 
-pushd src/nmath/standalone || exit
+echo "Entering src/nmath/standalone directory..."
+pushd src/nmath/standalone || { echo "Failed to enter src/nmath/standalone directory"; exit 1; }
 
-make --jobs="$(nproc)" shared
-make install
+# Determine number of cores for make command differently based on OS
+if [[ $(uname) == "Darwin" ]]; then
+    NUM_CORES=$(sysctl -n hw.ncpu)
+else
+    NUM_CORES=$(nproc)
+fi
 
-find "${PREFIX}"
+echo "Building with ${NUM_CORES} cores..."
+make --jobs="${NUM_CORES}" shared || { echo "Make failed"; exit 1; }
+echo "Installing..."
+make install || { echo "Make install failed"; exit 1; }
 
+find "${PREFIX}" || { echo "Find command failed"; exit 1; }
+
+echo "Exiting src/nmath/standalone directory..."
 popd || exit
+
+echo "Build script completed successfully."
