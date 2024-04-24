9979214835fb853a83197174048628413c78e336
t.fodran <t.fodran@astro.ru.nl>
2024-04-24 16:17:37 +0200

    Confirm screen now general

--- src/halfpipe/tui/utils/confirm_screen.py
+++ src/halfpipe/tui/utils/confirm_screen.py
@@ -9,15 +9,18 @@ from textual.widgets import Button, Label
 class Confirm(ModalScreen):
     CSS_PATH = ["tcss/confirm.tcss"]
 
-    def __init__(self) -> None:
+    def __init__(self, text="Are you sure?", left_button_text="Ok", right_button_text="Cancel") -> None:
         super().__init__()
+        self.text = text
+        self.left_button_text = left_button_text
+        self.right_button_text = right_button_text
 
     def compose(self) -> ComposeResult:
         yield Container(
-            Container(Label("Are you sure?", id="message"), classes="message_container"),
+            Container(Label(self.text, id="message"), classes="message_container"),
             Grid(
-                Button("Ok", variant="success", classes="button ok"),
-                Button("Cancel", variant="error", classes="button cancel"),
+                Button(self.left_button_text, variant="success", classes="button ok"),
+                Button(self.right_button_text, variant="error", classes="button cancel"),
                 classes="button_grid",
             ),
             id="confirm_screen",
