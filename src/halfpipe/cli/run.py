c6fd94218bea55391871d603784d182af20e3da2
Lea Waller <lea@lea.science>
2024-10-17 14:49:34 +0200

    Add todo for experimental flag

--- src/halfpipe/cli/run.py
+++ src/halfpipe/cli/run.py
@@ -10,12 +10,15 @@ from typing import Any, Mapping
 logger = logging.getLogger("halfpipe")
 
 
-def run_stage_ui(opts):
+def run_stage_ui(opts: Namespace):
     from ..ui import init_spec_ui
     from ..ui.components.config import Config as UIConfig
 
     UIConfig.fs_root = str(opts.fs_root)
-    opts.workdir = init_spec_ui(workdir=opts.workdir, debug=opts.debug)
+    if opts.experimental:
+        pass  # TODO
+    else:
+        opts.workdir = init_spec_ui(workdir=opts.workdir, debug=opts.debug)
 
 
 def run_stage_workflow(opts):
