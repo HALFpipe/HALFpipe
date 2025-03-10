48f6173a0f4bb7f699633cf8ecf07c200e4b121d
t.fodran <t.fodran@astro.ru.nl>
2025-03-10 18:02:27 +0100

    Adding new tests

---
+++ tests/tui/test_spec_file_load.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+from functools import partial
+from pathlib import Path
+from asyncio import sleep
+from textual import pilot
+from .pilot_functions import _load_data, _set_work_dir, set_non_bids_data, settable_scroll_screen_down, check_and_run_tab_refresh
+
+
+async def run_before(
+    pilot, data_path=None, spec_file_dir_path=None, feature_number=None, scroll_to_remaining_part=False
+) -> None:
+    # always reload the app first, there is some strange crossinteraction between tests, nothing else helped except using
+    # -n 2 flag for the pytest, i.e., running each test with a separate worker
+    feature_number = feature_number if feature_number is not None else None
+
+    pilot.app.reload_ui()
+    if isinstance(spec_file_dir_path, Path):
+        work_dir_path = str(spec_file_dir_path)
+        # work_dir_path = "/makethisfail/"
+
+    await _set_work_dir(pilot, work_dir_path, load_from_spec_file=True)
+    # For some reason the button remains focussed when I do it locally, but it is not focussed when it runs through CI,
+    # the tab should prevent this.
+    if feature_number is None:
+        await check_and_run_tab_refresh(pilot)
+        await settable_scroll_screen_down(pilot, 50)
+    else:
+        await sleep(10)
+        await pilot.press('f')
+        if scroll_to_remaining_part:
+            await pilot.click(offset=(15, feature_number))
+            await settable_scroll_screen_down(pilot, 28)
+        # select particular feature tab
+        await pilot.click(offset=(15, feature_number))
+        # await settable_scroll_screen_down(pilot, 50)
+        # click on the form area
+        await pilot.click(offset=(55, 10))
+        # scroll
+        await settable_scroll_screen_down(pilot, 15)
+
+
+
+def test_load_from_spec_file_f0(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 20) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f1(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 24) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f2(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 32) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f3(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 40) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f4(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 48) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+def test_load_from_spec_file_f5(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 28) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number, scroll_to_remaining_part=True)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f6(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 36) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number, scroll_to_remaining_part=True)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f7(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 40) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number, scroll_to_remaining_part=True)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+def test_load_from_spec_file_f8(snap_compare, start_app, spec_file_dir_path: Path, feature_number: int = 48) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path, feature_number=feature_number, scroll_to_remaining_part=True)
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+
+# Function to compare files since the 10th line
+def compare_files(file1, file2):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        # Skip the first 4 lines in both files
+        f1_lines = f1.readlines()[4:-2]
+        f2_lines = f2.readlines()[4:-2]
+        # Return True if the remaining lines are the same, else False
+        return f1_lines == f2_lines
+
+def test_load_from_spec_file_resave_spec_file(snap_compare, start_app, spec_file_dir_path: Path) -> None:
+    """Check whether one can set the working directory."""
+    run_before_with_extra_args = partial(
+        run_before, spec_file_dir_path=spec_file_dir_path)
+
+    file1 = spec_file_dir_path / "spec.json"
+    file2 = spec_file_dir_path / "spec_reference.json"
+
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+    assert compare_files(file1, file2) == True
+
---
+++ tests/tui/test_task_global_models.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+from functools import partial
+from pathlib import Path
+
+from .pilot_functions import (
+    _load_data,
+    _set_work_dir,
+    add_contrast_value_column,
+    add_new_feature,
+    check_and_run_tab_refresh,
+    delete_column,
+    deselect_conditions,
+    preprocessing_options,
+    remove_confounds,
+    scroll_screen_down,
+    select_images,
+    settable_scroll_screen_down,
+    _select_covariates_spreadsheet,
+    _select_group_level_models_cutoffs_values
+)
+
+
+async def run_before(pilot, data_path=None, work_dir_path=None, covariant_spreadsheet_path=None, stage=None) -> None:
+    # always reload the app first, there is some strange crossinteraction between tests, nothing else helped except using
+    # -n 2 flag for the pytest, i.e., running each test with a separate worker
+    how_much_down = 0
+
+    pilot.app.reload_ui()
+    if isinstance(data_path, Path):
+        data_path = str(data_path)
+    if isinstance(work_dir_path, Path):
+        work_dir_path = str(work_dir_path)
+    if isinstance(covariant_spreadsheet_path, Path):
+        covariant_spreadsheet_path = str(covariant_spreadsheet_path)
+    print("----------------------------", data_path, work_dir_path, covariant_spreadsheet_path)
+
+    # Define functions to execute based on stage requirements
+    async def add_feature_related_tasks():
+        await add_new_feature(pilot)
+        await select_images(pilot)
+
+    async def add_intercept_only_model():
+        await add_new_feature(pilot, tab_type='g', feature_type="intercept_only", label="intercept_only_1")
+
+    async def add_linear_model():
+        await add_new_feature(pilot, tab_type='g', feature_type="linear_model", label="linear_model_1")
+
+    async def select_aggregate():
+        await pilot.click(offset=(72, 15))
+
+    async def select_group_level_models_cutoffs_values():
+        await _select_group_level_models_cutoffs_values(pilot)
+
+    async def select_covariates_spreadsheet():
+        await _select_covariates_spreadsheet(pilot, covariant_spreadsheet_path)
+        # scroll to see more
+        # await settable_scroll_screen_down(pilot, 29)
+
+    async def duplicate():
+        await pilot.click(offset=(10, 12))
+        # click on the form area
+        await pilot.click(offset=(55, 10))
+        # scroll to see more
+        await settable_scroll_screen_down(pilot, 29)
+
+    async def final_stage_tasks():
+        # press check and run also manually
+        await pilot.click(offset=(108, 4))
+        await check_and_run_tab_refresh(pilot)
+        await settable_scroll_screen_down(pilot, 60)
+        # os.rename(Path(work_dir_path) / "spec.json", Path(work_dir_path) / f"spec_{stage}.json")
+
+    # Map stages to the tasks they should trigger
+    tasks_by_stage = {
+        "intercept_only_at_group_level_models_tab": [add_feature_related_tasks, add_intercept_only_model, select_aggregate, select_group_level_models_cutoffs_values],
+        "intercept_only_at_spec_preview": [add_feature_related_tasks, add_intercept_only_model, select_aggregate, select_group_level_models_cutoffs_values, final_stage_tasks],
+        "intercept_only_at_group_level_models_tab_duplicate": [add_feature_related_tasks, add_intercept_only_model, select_aggregate,
+                                                     select_group_level_models_cutoffs_values, duplicate],
+        "linear_model_at_group_level_models_tab": [add_feature_related_tasks, add_linear_model, select_covariates_spreadsheet],
+        "linear_model_at_group_level_models_tab_duplicate": [add_feature_related_tasks, add_linear_model, select_covariates_spreadsheet, duplicate],
+        "linear_model_at_spec_preview": [add_feature_related_tasks, add_linear_model, select_covariates_spreadsheet, final_stage_tasks],
+    }
+    if stage == "at_spec_preview":
+        how_much_down = 60
+    elif stage == "duplicate_at_spec_preview":
+        how_much_down = 36
+
+    # Execute tasks based on the specified stage
+    # set work dir
+    await _set_work_dir(pilot, work_dir_path)
+    # set data dir
+    await _load_data(pilot, data_path)
+    for task in tasks_by_stage[stage]:
+        await task()
+
+# 1
+def test_intercept_only_at_global_models_tab(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path) -> None:
+    """Add Task-based feature, add intercept only group level model, make cutoff choices"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, stage="intercept_only_at_group_level_models_tab"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+# 2
+def test_intercept_only_at_group_level_models_tab_duplicate(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path) -> None:
+    """Add Task-based feature, add intercept only group level model, make cutoff choices, then duplicate"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, stage="intercept_only_at_group_level_models_tab_duplicate"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+# 3
+def test_intercept_only_at_spec_preview(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path) -> None:
+    """Add Task-based feature, add intercept only group level model, make cutoff choices, check spec file preview"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, stage="intercept_only_at_spec_preview"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+# 4
+def test_linear_model_at_group_level_models_tab(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path, covariant_spreadsheet_path:Path) -> None:
+    """Add Task-based feature, add linear group level model, add spreadsheet"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, covariant_spreadsheet_path=covariant_spreadsheet_path, stage="linear_model_at_group_level_models_tab"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+
+# 5
+def test_linear_model_at_group_level_models_tab_duplicate(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path, covariant_spreadsheet_path:Path) -> None:
+    """Add Task-based feature, add linear group level model, add spreadsheet and duplicate"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, covariant_spreadsheet_path=covariant_spreadsheet_path, stage="linear_model_at_group_level_models_tab_duplicate"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
+
+# 6
+def test_linear_model_at_spec_preview(snap_compare, start_app, work_dir_path: Path, downloaded_data_path: Path, covariant_spreadsheet_path:Path) -> None:
+    """Add Task-based feature, add linear group level model, add spreadsheet and check spec preview"""
+    run_before_with_extra_args = partial(
+        run_before, data_path=downloaded_data_path, work_dir_path=work_dir_path, covariant_spreadsheet_path=covariant_spreadsheet_path, stage="linear_model_at_spec_preview"
+    )
+    assert snap_compare(app=start_app, terminal_size=(204, 53), run_before=run_before_with_extra_args)
