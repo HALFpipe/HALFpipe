2c8f4bfc9ae41697b520fd9d4eee3a704a03602c
t.fodran <t.fodran@astro.ru.nl>
2025-03-20 00:25:41 +0100

    try with hardcoded

--- tests/tui/conftest.py
+++ tests/tui/conftest.py
@@ -11,17 +11,17 @@ from halfpipe.tui.base import MainApp  # Ensure path aligns with your project st
 
 from .create_mock_bids_dataset import create_bids_data
 
+CURRENT_DIR = Path('/home/runner/actions-runner/_work/HALFpipe/HALFpipe/tests/tui/')
 
 @pytest.fixture(scope="session", autouse=True)
 def copy_jinja2_file():
     """Copy a file before tests start. This is just a hot fix because somehow the resources directory
     is delete during the docker build."""
-    source_file = "./snapshot_report_template.jinja2"
+    source_file = CURRENT_DIR / "snapshot_report_template.jinja2"
     destination = Path("/opt/conda/envs/fmriprep/lib/python3.11/site-packages/resources/")
 
     destination.mkdir(parents=True, exist_ok=True)
     shutil.copy(source_file, destination / "snapshot_report_template.jinja2")
-
 
 # Custom fixture that returns a specific path, this is needed so that the path in the snapshot is always the same
 # If the path was variable then the snapshot would yield failure.
@@ -91,7 +91,7 @@ def work_dir_path(fixed_tmp_path) -> Path:
 
 @pytest.fixture(scope="session")
 def spec_file_dir_path(fixed_tmp_path) -> Path:
-    source_dir = "./spec_file_for_load_test"
+    source_dir = CURRENT_DIR / "spec_file_for_load_test"
     destination_dir = fixed_tmp_path / "spec_file_for_load_test/"
     if os.path.exists(destination_dir):
         shutil.rmtree(destination_dir)
@@ -101,7 +101,7 @@ def spec_file_dir_path(fixed_tmp_path) -> Path:
 
 @pytest.fixture(scope="session")
 def covariant_spreadsheet_path(fixed_tmp_path) -> Path:
-    source_file = "./Covariates.xlsx"
+    source_file = CURRENT_DIR / "Covariates.xlsx"
     destination_file = fixed_tmp_path / "Covariates.xlsx"
     if os.path.exists(destination_file):
         os.remove(destination_file)
@@ -128,3 +128,5 @@ def event_path_pattern(downloaded_data_path) -> Path:
 @pytest.fixture(scope="function")
 def start_app():
     return MainApp()
+
+
