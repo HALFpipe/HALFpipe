974d984862af0bf5a18284d9b7f9071dd19ef608
t.fodran <t.fodran@astro.ru.nl>
2025-04-01 02:13:13 +0200

    change order

--- src/halfpipe/model/tags/schema.py
+++ src/halfpipe/model/tags/schema.py
@@ -18,7 +18,7 @@ schemas = [
     ResultdictTagsSchema,
 ]
 
-entities_list = ["run", "task", "ses", "sub"]
+entities_list = ["run", "ses", "task", "sub"]
 for schema in schemas:  # automatically add other entities
     instance = schema()
     for key in instance.fields.keys():
