754a73be1a5d8747e9643931a264952f0ffb67dd
Lea Waller <lea.waller@charite.de>
2025-04-07 16:31:47 +0200

    Load derivatives directories in parallel

--- src/halfpipe/collect/derivatives.py
+++ src/halfpipe/collect/derivatives.py
@@ -9,6 +9,7 @@ from typing import Generator, Iterable
 from ..file_index.bids import BIDSIndex
 from ..result.base import ResultDict
 from ..result.bids.images import load_images
+from ..utils.multiprocessing import make_pool_or_null_context
 from ..utils.path import AnyPath, recursive_list_directory
 
 
@@ -41,11 +42,29 @@ def find_derivatives_directories(name: str, path: str | AnyPath, max_depth: int 
         yield path
 
 
+def create_index(derivatives_directory: AnyPath) -> BIDSIndex:
+    return BIDSIndex().put(derivatives_directory)
+
+
 def collect_halfpipe_derivatives(paths: Iterable[str | AnyPath], max_depth: int = 1, num_threads: int = 1) -> list[ResultDict]:
     index = BIDSIndex()
-    for path in paths:
-        for derivatives_directory in find_derivatives_directories("halfpipe", path, max_depth=max_depth):
-            index.put(derivatives_directory)
+
+    derivatives_directories = {
+        derivatives_directory
+        for path in paths
+        for derivatives_directory in find_derivatives_directories("halfpipe", path, max_depth=max_depth)
+    }
+
+    cm, iterator = make_pool_or_null_context(
+        derivatives_directories,
+        callable=create_index,
+        num_threads=num_threads,
+        chunksize=None,
+    )
+
+    with cm:
+        for derivatives_directory_index in iterator:
+            index.update(derivatives_directory_index)
 
     results = load_images(index, num_threads=num_threads)
     return results
