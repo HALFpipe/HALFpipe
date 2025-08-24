944a1411347780d7bcf75e777d4b0ee097c8c115
F-Tomas <fodran.tomas@gmail.com>
2025-08-24 15:46:47 +0200

    fix

--- src/halfpipe/tui/working_directory/base.py
+++ src/halfpipe/tui/working_directory/base.py
@@ -153,11 +153,13 @@ spec.json file it is possible to load the therein configuration.",
             self.get_widget_by_id("work_dir_file_browser").styles.border = ("solid", "red")
             ctx.workdir = None
 
+
+
     @on(FileBrowser.Changed, "#fs_license_file_browser")
     async def _on_fs_license_file_browser_changed(self, message: Message) -> None:
-        self.evaluate_fs_license(message.selected_path)
+        await self.evaluate_fs_license(message.selected_path)
 
-    def evaluate_fs_license(self, fs_file_path) -> None:
+    async def evaluate_fs_license(self, fs_file_path) -> None:
 
         os.environ["FS_LICENSE"] = fs_file_path
         if not check_valid_fs_license():
@@ -280,6 +282,8 @@ overwrite the working directory and start a new analysis?",
             await existing_spec_file_decision(result)
 
 
+        # ALWAYS trigger license check at the end
+        self._evaluate_license_worker(str(selected_path))
 
 
 
@@ -308,14 +312,13 @@ overwrite the working directory and start a new analysis?",
                 self._mount_file_panels()
             if event.worker.name == "file_panels_worker":
                 self._mount_models()
-            if event.worker.name == "work_dir_path_passed_worker":
-                # freesurfer license block
-                selected_path = self.get_widget_by_id("work_dir_file_browser").selected_path
-                full_fs_license_path = os.path.join(selected_path, "license.txt")
-                if not self.fs_license_file_found:
-                    self.get_widget_by_id("fs_license_file_browser").update_input(full_fs_license_path, send_message=False)
-                    self.evaluate_fs_license(full_fs_license_path)
 
+    @work(exclusive=True, name="license_worker")
+    async def _evaluate_license_worker(self, selected_path: str):
+        full_fs_license_path = os.path.join(selected_path, "license.txt")
+        if not self.fs_license_file_found:
+            self.get_widget_by_id("fs_license_file_browser").update_input(full_fs_license_path, send_message=False)
+            await self.evaluate_fs_license(full_fs_license_path)
 
     async def _load_from_spec(self):
         # Go to Stage 1 of the loading process
