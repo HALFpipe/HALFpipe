22273e35fd75e511867f0463e1d2857a4726e52c
F-Tomas <fodran.tomas@gmail.com>
2025-09-26 11:06:26 +0200

    New UI - add batch script generation (#847)
    * Implementing batch script option into the run tab plus some refactoring
    
    * styling
    
    * extend custom opts in batch dataclass
    
    * some more work done
    
    * first checkpoint
    
    * second checkpoint
    
    * adding diagnostis tcss file
    
    * set env for freesurfer
    
    * some more work on freesurfer license
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * update diagnostic tab and wire everything up
    
    * fix
    
    * throw error modal when fs install is missing
    
    * Updating and resolving conflicts with main
    
    * button changes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * accept also empty field
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/halfpipe/cli/run.py
+++ src/halfpipe/cli/run.py
@@ -17,10 +17,21 @@ def run_stage_ui(opts: Namespace):
     UIConfig.fs_root = str(opts.fs_root)
 
     if opts.tui:
+        import os
+
         from ..tui.base import MainApp
 
+        if opts.tui_dev:
+            os.environ["TEXTUAL"] = "devtools,debug"
+
         app = MainApp()
-        opts.workdir = app.run()
+        opts_tui = app.run()
+        # update opts based on selection in tui
+        if opts_tui:
+            for k in vars(opts).keys() & opts_tui.keys():
+                setattr(opts, k, opts_tui[k])
+            logger.info(f"The options selected in TUI are: {opts}")
+
         if opts.workdir is False:
             import os
 
