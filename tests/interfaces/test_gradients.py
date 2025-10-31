0f93b0a9f1506cd767e0d8f3fa113e0580e6faf7
Annabelle Harvey <annabelle.ahrv@gmail.com>
2025-10-30 23:58:01 -0400

    fixed traits issue, needed to enable usedefaults flag

--- tests/interfaces/test_gradients.py
+++ tests/interfaces/test_gradients.py
@@ -1,65 +1,61 @@
+import os
 import pytest
 import numpy as np
+import pandas as pd
 
 from halfpipe.interfaces.gradients import Gradients
 from traits.trait_errors import TraitError
 
 def test_brainspace_install():
-    """ Test if brainspace is installed."""
+    """ Test if brainspace is installed. """
     try:
         import brainspace
     except ImportError:
         raise ImportError(f'Install "brainspace".')
 
 def test_inputs_string():
+    """ Test if traits are checking input correctly. """
     g = Gradients()
     with pytest.raises(TraitError):
         g.inputs.n_components = 'fails'
 
 def test_inputs_array():
+    """ Test if traits are checking input correctly. """
     g = Gradients()
 
     g.inputs.x = np.random.randn(5,5)
 
 def test_inputs_union():
+    """ Test if traits are checking input correctly. """
     g = Gradients()
 
     g.inputs.x = [np.random.randn(5,5),np.random.randn(5,5)]
 
+def test_gradient_single_random_array(tmp_path):
+    """ Test brainspace functions on random array & that traits are filling in default values. """
+    os.chdir(str(tmp_path))
 
-# FAILING
-# traits is not applying default values??
-# needs to be run by called by nipype somehow?
-
-    # Investigate! known to nipype:
-    """
-    The functions that pop-up the Traits GUIs, edit_traits and
-configure_traits, were failing because all of our inputs default to
-Undefined deep and down in traits/ui/wx/list_editor.py it checks for
-the len() of the elements of the list.  The _Undefined class in traits
-does not define the __len__ method and would error.  I tried defining
-our own Undefined and even subclassing Undefined, but both of those
-failed with a TraitError in our initializer when we assign the
-Undefined to the inputs because of an incompatible type:"""
-def test_gradient_single_random_array():
     g = Gradients()
 
     g.inputs.x = np.random.randn(100,100)
-
-    # TRAITS NOT PUTTING IN DEFAULTS
-    g.inputs.n_components = 10
-    g.inputs.approach = 'dm'
-
-    g.inputs.kernel = None
-    g.inputs.random_state = None
-    g.inputs.alignment = None
-
-    g.inputs.gamma = None
-    g.inputs.sparsity = 0.9
-    g.inputs.n_iter = 10
-    g.inputs.reference = None
 
     g._run_interface('fake runtime')
 
     assert g._gradients.shape[0] == 100
     assert g._gradients.shape[1] == 10
+
+def test_outputs(tmp_path):
+    """ Test brainspace functions on random array & that traits are filling in default values. """
+    os.chdir(str(tmp_path))
+
+    g = Gradients()
+
+    g.inputs.x = np.random.randn(100,100)
+
+    g._run_interface('fake runtime')
+
+    outputs = g._list_outputs()
+    
+    grads = pd.read_csv(outputs['gradients'], delimiter='\t')
+
+    assert grads.shape[1] == 10
