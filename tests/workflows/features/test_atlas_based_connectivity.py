62bf7381e4ec82a7b46002ffab8d2c20d835814c
Annabelle Harvey <annabelle.ahrv@gmail.com>
2025-11-20 19:05:53 +0000

    breakpoint

--- tests/workflows/features/test_atlas_based_connectivity.py
+++ tests/workflows/features/test_atlas_based_connectivity.py
@@ -2,43 +2,86 @@ import os
 import pytest
 
 import numpy as np
-from nilearn.datasets import fetch_atlas_msdl, fetch_development_fmri
+
+from templateflow.api import get as get_template
+from nilearn.datasets import fetch_development_fmri
 from nilearn.masking import compute_brain_mask
+from nilearn.image import load_img
+from nilearn.image import resample_to_img
+
+#import nibabel as nib
+#from nilearn.image import new_img_like
 
 from halfpipe.utils.nipype import run_workflow
 from halfpipe.model.feature import Feature
 from halfpipe.workflows.features.atlas_based_connectivity import init_atlas_based_connectivity_wf
+from halfpipe.resource import get as get_resource
+
+from ...resource import setup as setup_test_resources
 
 # nice when there's internet
-#@pytest.fixture(scope="module")
-#def test_data(tmp_path_factory):
-#    tmp = tmp_path_factory.mktemp("data")
-#    atlas = fetch_atlas_msdl(data_dir=tmp)
-#    atlas_filename = atlas["maps"]
-#    labels = atlas["labels"]
-#    data = fetch_development_fmri(n_subjects=1, data_dir=tmp)
-#    mask_img = compute_brain_mask(data.func[0])
-#    mask_img.to_filename(tmp / 'mask.nii.gz')
-#    return atlas_filename, labels, data, tmp, tmp / 'mask.nii.gz'
-
 @pytest.fixture(scope="module")
-def tmp_dir(tmp_path_factory):
+def test_data(tmp_path_factory):
     tmp = tmp_path_factory.mktemp("data")
-    return tmp
 
-def test_atlas_based_connectivity_wf(tmp_dir):
-    # atlas_filename, labels, data, tmp_dir, mask_filename = test_data
-    # bold_filename = data['func'][0]
+    # get template
+    template_filename = get_template(
+                'MNI152NLin2009cAsym',
+                resolution=2,
+                desc=None,
+                suffix="T1w",
+            )
+    template = load_img(template_filename)
+    
+    # get func data
+    data = fetch_development_fmri(n_subjects=1)
+    data_resampled = resample_to_img(data.func[0],template)
+    bold_filename = tmp / 'resampled_func.nii.gz'
+    data_resampled.to_filename(bold_filename)
+
+    # get mask
+    mask_img = compute_brain_mask(bold_filename)
+    mask_filename = tmp / "mask.nii.gz"
+    mask_img.to_filename(mask_filename)
+
+    # hard coded path within container
+    atlas_filename="/halfpipe_dev/test_data/atlases/atlas-DesikanKilliany_dseg.nii.gz"
+
+    #bold_filename = get_resource("sub-50005_task-rest_bold_space-MNI152NLin2009cAsym_preproc.nii.gz")
+    # online resource
+    #setup_test_resources()
+
+    # make mask
+    #image_data = nib.nifti1.load(bold_filename).get_fdata()
+    #mask_data = image_data.std(axis=3) > 0
+    #mask_image = new_img_like(image, data=mask_data, copy_header=True)
+    #nib.loadsave.save(mask_image, mask_filename)
+
+
+    #zooms: tuple[float, ...] = image.header.get_zooms()
+
+    #sampling_period: float = float(zooms[3])  # type: ignore
+    #sampling_frequency = 1 / sampling_period
+
+    return atlas_filename, bold_filename, mask_filename, tmp
+
+#@pytest.fixture(scope="module")
+#def tmp_dir(tmp_path_factory):
+#    tmp = tmp_path_factory.mktemp("data")
+#    return tmp
+
+def test_atlas_based_connectivity_wf(test_data):
+    atlas_filename, bold_filename, mask_filename, tmp = test_data
 
     # Hard coding paths (within container) - no internet on interactive node 
-    mask_filename = '/halfpipe_dev/test_data/conn_test/mask.nii.gz'
-    atlas_filename = '/halfpipe_dev/test_data/conn_test/resampled_atlas.nii.gz'
-    bold_filename = '/halfpipe_dev/test_data/conn_test/resampled_func.nii.gz'
+    # mask_filename = '/halfpipe_dev/test_data/conn_test/mask.nii.gz'
+    # atlas_filename="/halfpipe_dev/test_data/atlases/atlas-DesikanKilliany_dseg.nii.gz"
+    # bold_filename = '/halfpipe_dev/test_data/conn_test/resampled_func.nii.gz'
 
-    os.chdir(tmp_dir)
+    os.chdir(tmp)
     rng = np.random.default_rng(0)
 
-    workdir = tmp_dir / "workdir"
+    workdir = tmp / "workdir"
     workdir.mkdir(exist_ok=True)
 
     # Initialize a wf
@@ -52,38 +95,11 @@ def test_atlas_based_connectivity_wf(tmp_dir):
     inputnode = wf.get_node("inputnode")
     assert inputnode is not None
 
-    inputnode.inputs.atlas_names = ['msdl'] # ran up to at least resampling node no fail with this value as non-matching list (?)
+    inputnode.inputs.atlas_names = ['DesikanKilliany_dseg'] # ran up to at least resampling node no fail with this value as non-matching list (?)
 
     min_region_coverage = 0.8 # default value
 
     # PASS INPUTS TO INPUTS NODE
-    # V1 pass nothing outside constructor for wf
-        # ValueError: TSNR requires a value for input 'in_file'. For a list of required inputs, see TSNR.help()
-        # 2847.36s
-        # SOOOOOO SLOOOOOW
-    # V2 pass bold file
-        # ValueError: ConnectivityMeasure requires a value for input 'mask_file'. For a list of required inputs, see ConnectivityMeasure.help()
-    # V3 pass mask file
-        # nipype.pipeline.engine.nodes.NodeExecutionError: Exception raised while executing Node _connectivitymeasure0.
-        # ValueError: Atlas image and data image must have the same shape
-            # error should come up earlier: put everything in input node & check
-            # Lea: resampling of bold is done once in fmri prep pipeline, this assumes that only atlas needs to be resampled - so inputs here must have same shape
-            # Hard printed the shapes
-                # ATLAS:  (97, 115, 97, 39)
-                # VOLUME:  (50, 59, 50) (vs in nilearn (50, 59, 50, 168) what happens to 4th dim?)
-            # Cant find input information in the halfpipe debugging outputs
-            # but useful to know that inputs were passed 
-            # bypassing proper inputs probably broke logging
-    # V3 Change to native space (rather than attempt to understand desired spaces etc)
-        # whoop that was more confusing
-    # V4 I'm passing an atlas and a func file that I resampled already to the same template 
-    #   (seems like it should be same as what is in halfpipe MNI152NLin2009cAsym resolution 2 but output dimensions are different e.g. atlas (99, 117, 95, 39) )
-    #   passes!
-    # V5 found a bug in previous test (was passing the same file as atlas and func yikes) & lea suggested slow test time was due to prob. atlas so changed to destrieux
-    # fails!  ValueError: Atlas image and data image must have the same shape
-    # very confused, i had thought func data would always be 4d and a non-prob atlas should be 3d 
-
-
     #  "tags"
     #  "vals"
     #  "metadata",
@@ -92,7 +108,7 @@ def test_atlas_based_connectivity_wf(tmp_dir):
     inputnode.inputs.bold = bold_filename # path to .nii.gz file
 
     #   "mask",
-    inputnode.inputs.mask = mask_filename # what exactly is a mask_file? : output from fMRI_prep, path to .nii.gz file w mask
+    inputnode.inputs.mask = mask_filename # mask_file output from fMRI_prep, path to .nii.gz file w/ mask
 
     #   "repetition_time"
 
@@ -103,4 +119,15 @@ def test_atlas_based_connectivity_wf(tmp_dir):
     #   "std2anat_xfm",
     #   "bold_ref_anat",
 
-    run_workflow(wf)
+    graph = run_workflow(wf)
+
+    # Awkward to access node information due to nipype funtion (runs a copy of workflow)
+    node_names = [n.names for n in graph.nodes]
+    #(out_file,) = merge.result.outputs.out_file
+
+    #image = nib.nifti1.load(out_file)
+    #image_data = image.get_fdata()
+
+    #_, after = welch(image_data[mask_data, :].ravel(), sampling_frequency)
+    #print node_names
+    breakpoint()
