0033e038f619339754f8bf28b89f9eb2086bf58b
Annabelle Harvey <annabelle.ahrv@gmail.com>
2025-11-27 21:57:20 +0000

    restructured to match gradients wf test

--- tests/workflows/features/test_atlas_based_connectivity.py
+++ tests/workflows/features/test_atlas_based_connectivity.py
@@ -19,7 +19,6 @@ from halfpipe.resource import get as get_resource
 
 from ...resource import setup as setup_test_resources
 
-# nice when there's internet
 @pytest.fixture(scope="module")
 def test_data(tmp_path_factory):
     tmp = tmp_path_factory.mktemp("data")
@@ -47,28 +46,8 @@ def test_data(tmp_path_factory):
     # hard coded path within container
     atlas_filename="/halfpipe_dev/test_data/atlases/atlas-DesikanKilliany_dseg.nii.gz"
 
-    #bold_filename = get_resource("sub-50005_task-rest_bold_space-MNI152NLin2009cAsym_preproc.nii.gz")
-    # online resource
-    #setup_test_resources()
-
-    # make mask
-    #image_data = nib.nifti1.load(bold_filename).get_fdata()
-    #mask_data = image_data.std(axis=3) > 0
-    #mask_image = new_img_like(image, data=mask_data, copy_header=True)
-    #nib.loadsave.save(mask_image, mask_filename)
-
-
-    #zooms: tuple[float, ...] = image.header.get_zooms()
-
-    #sampling_period: float = float(zooms[3])  # type: ignore
-    #sampling_frequency = 1 / sampling_period
-
     return atlas_filename, bold_filename, mask_filename, tmp
 
-#@pytest.fixture(scope="module")
-#def tmp_dir(tmp_path_factory):
-#    tmp = tmp_path_factory.mktemp("data")
-#    return tmp
 
 def test_atlas_based_connectivity_wf(test_data):
     atlas_filename, bold_filename, mask_filename, tmp = test_data
@@ -84,50 +63,41 @@ def test_atlas_based_connectivity_wf(test_data):
     workdir = tmp / "workdir"
     workdir.mkdir(exist_ok=True)
 
+    feat = Feature(
+        "atlas_based_connectivity", # name
+        "atlas_based_connectivity", # type
+ 
+        # kwargs
+        **{"atlases":['DesikanKilliany_dseg'],
+        "min_region_coverage":0.8,
+        }
+        )
+
     # Initialize a wf
-    # Features make no sense at all, doing this bypassing them bc I simply cannot understand what they want or what they do
     wf = init_atlas_based_connectivity_wf(workdir,
-                                            #optional arguments but don't understand yet
+                                        feat,
                                         atlas_files = [atlas_filename],
-                                        atlas_spaces = ['MNI152NLin2009cAsym'], # FOUND IT IN CONSTANTS IT WANTS RESOLUTION 2
+                                        atlas_spaces = ['MNI152NLin2009cAsym'], # needs resolution 2 (ref: workflows.constants)
                                             )
     wf.base_dir = workdir
     inputnode = wf.get_node("inputnode")
     assert inputnode is not None
 
-    inputnode.inputs.atlas_names = ['DesikanKilliany_dseg'] # ran up to at least resampling node no fail with this value as non-matching list (?)
-
-    min_region_coverage = 0.8 # default value
-
     # PASS INPUTS TO INPUTS NODE
-    #  "tags"
-    #  "vals"
-    #  "metadata",
 
-    #   V2 - inputnode "bold" connected to tsnr "in_file" so should be enough to pass bold to inputnode?
+    # inputnode "bold" connected to tsnr "in_file"
     inputnode.inputs.bold = bold_filename # path to .nii.gz file
-
-    #   "mask",
     inputnode.inputs.mask = mask_filename # mask_file output from fMRI_prep, path to .nii.gz file w/ mask
-
-    #   "repetition_time"
-
-    #   "atlas_names", # bypassed these but should've been in constructor inside Feature
-    #   "atlas_files", # passed these in constructor
-    #   "atlas_spaces", # passed these in constructor
-        # resampling to native space
-    #   "std2anat_xfm",
-    #   "bold_ref_anat",
-
+   
     graph = run_workflow(wf)
 
     # Awkward to access node information due to nipype funtion (runs a copy of workflow)
-    node_names = [n.names for n in graph.nodes]
-    #(out_file,) = merge.result.outputs.out_file
+    node_names = [n.name for n in graph.nodes]
+    resultdict_datasink = [n for n in graph.nodes if n.name == 'resultdict_datasink'][0]
+    make_resultdicts = [n for n in graph.nodes if n.name == 'make_resultdicts'][0]
 
-    #image = nib.nifti1.load(out_file)
-    #image_data = image.get_fdata()
+    print(resultdict_datasink.inputs)
 
-    #_, after = welch(image_data[mask_data, :].ravel(), sampling_frequency)
-    #print node_names
-    breakpoint()
+    print(make_resultdicts.outputs)
+ 
+    assert False
