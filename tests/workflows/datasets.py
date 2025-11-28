047bbda9921e389df0e927ab053ce19e822c0b1a
lalalavi <violeta.cespedes@live.com>
2025-11-28 13:01:49 +0100

    remove neuromark and include reports folder

--- tests/workflows/datasets.py
+++ tests/workflows/datasets.py
@@ -55,28 +55,28 @@ class Dataset:
 
 
 datasets: list[Dataset] = [
-    Dataset(
-        name="on_harmony",  # single-band scan
-        openneuro_id="ds004712",
-        openneuro_url="https://github.com/OpenNeuroDatasets/ds004712/blob/master",
-        url="https://github.com/OpenNeuroDatasets/ds004712.git",
-        paths=[
-            "sub-13192/ses-NOT3GEM001/func/sub-13192_ses-NOT3GEM001_task-rest_acq-resopt2_bold.nii.gz",
-            "sub-13192/ses-NOT3GEM001/anat/sub-13192_ses-NOT3GEM001_T1w.json",
-            "sub-13192/ses-NOT3GEM001/anat/sub-13192_ses-NOT3GEM001_T1w.nii.gz",
-        ],
-    ),
-    Dataset(
-        name="emory",  # multiband
-        openneuro_id="ds003540",
-        openneuro_url="https://github.com/OpenNeuroDatasets/ds003540/blob/master",
-        url="https://github.com/OpenNeuroDatasets/ds003540.git",
-        paths=[
-            "sub-01/anat/sub-01_T1w.nii.gz",
-            "sub-01/func/sub-01_task-rest_acq-MB8_bold.nii.gz",
-            "sub-01/func/sub-01_task-rest_acq-MB8_sbref.nii.gz",
-        ],
-    ),
+    # Dataset(
+    #     name="on_harmony",  # single-band scan
+    #     openneuro_id="ds004712",
+    #     openneuro_url="https://github.com/OpenNeuroDatasets/ds004712/blob/master",
+    #     url="https://github.com/OpenNeuroDatasets/ds004712.git",
+    #     paths=[
+    #         "sub-13192/ses-NOT3GEM001/func/sub-13192_ses-NOT3GEM001_task-rest_acq-resopt2_bold.nii.gz",
+    #         "sub-13192/ses-NOT3GEM001/anat/sub-13192_ses-NOT3GEM001_T1w.json",
+    #         "sub-13192/ses-NOT3GEM001/anat/sub-13192_ses-NOT3GEM001_T1w.nii.gz",
+    #     ],
+    # ),
+    # Dataset(
+    #     name="emory",  # multiband
+    #     openneuro_id="ds003540",
+    #     openneuro_url="https://github.com/OpenNeuroDatasets/ds003540/blob/master",
+    #     url="https://github.com/OpenNeuroDatasets/ds003540.git",
+    #     paths=[
+    #         "sub-01/anat/sub-01_T1w.nii.gz",
+    #         "sub-01/func/sub-01_task-rest_acq-MB8_bold.nii.gz",
+    #         "sub-01/func/sub-01_task-rest_acq-MB8_sbref.nii.gz",
+    #     ],
+    # ),
     Dataset(
         name="panikratova",  # New 1.5 Tesla
         openneuro_id="ds002422",
@@ -89,24 +89,24 @@ datasets: list[Dataset] = [
             "T1w.json",
         ],
     ),
-    Dataset(
-        name="sleepy_brain",  # Has fieldmaps
-        openneuro_id="ds000201",
-        openneuro_url="https://github.com/OpenNeuroDatasets/ds000201/blob/master/",
-        url="https://github.com/OpenNeuroDatasets/ds000201.git",
-        paths=[
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude2.nii.gz",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude1.nii.gz",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_phase1.nii.gz",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_phase2.nii.gz",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude1.json",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude2.json",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_phase1.json",
-            "sub-9040/ses-1/fmap/sub-9040_ses-1_phase2.json",
-            "sub-9040/ses-1/anat/sub-9040_ses-1_T1w.json",
-            "sub-9040/ses-1/anat/sub-9040_ses-1_T1w.nii.gz",
-            "sub-9040/ses-1/func/sub-9040_ses-1_task-rest_bold.nii.gz",
-            "sub-9040/ses-1/func/sub-9040_ses-1_task-rest_bold.json",
-        ],
-    ),
+    # Dataset(
+    #     name="sleepy_brain",  # Has fieldmaps
+    #     openneuro_id="ds000201",
+    #     openneuro_url="https://github.com/OpenNeuroDatasets/ds000201/blob/master/",
+    #     url="https://github.com/OpenNeuroDatasets/ds000201.git",
+    #     paths=[
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude2.nii.gz",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude1.nii.gz",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_phase1.nii.gz",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_phase2.nii.gz",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude1.json",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_magnitude2.json",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_phase1.json",
+    #         "sub-9040/ses-1/fmap/sub-9040_ses-1_phase2.json",
+    #         "sub-9040/ses-1/anat/sub-9040_ses-1_T1w.json",
+    #         "sub-9040/ses-1/anat/sub-9040_ses-1_T1w.nii.gz",
+    #         "sub-9040/ses-1/func/sub-9040_ses-1_task-rest_bold.nii.gz",
+    #         "sub-9040/ses-1/func/sub-9040_ses-1_task-rest_bold.json",
+    #     ],
+    # ),
 ]
--- tests/workflows/test_consistency.py
+++ tests/workflows/test_consistency.py
@@ -6,7 +6,6 @@ import zipfile
 from datetime import datetime
 from multiprocessing import cpu_count
 from pathlib import Path
-
 import pytest
 from fmriprep import config
 
@@ -206,6 +205,7 @@ def test_extraction(dataset: Dataset, tmp_path: Path, pcc_mask: Path):
 
     dataset_file = dataset.download(tmp_path)
     spec = make_spec(dataset_files=[dataset_file], pcc_mask=pcc_mask, test_settings=settings_list)
+    spec.features = [feature for feature in spec.features if feature.type != "gig_ica"]     # Drop gig-ica feature
     config.nipype.omp_nthreads = cpu_count()
     save_spec(spec, workdir=tmp_path)
 
@@ -261,7 +261,9 @@ def test_extraction(dataset: Dataset, tmp_path: Path, pcc_mask: Path):
         tsnr_fmriprep = index.get(sub=sub, suffix="boldmap", datatype="func", stat="tsnr")
         confounds_sidecar = index.get(sub=sub, suffix="timeseries", datatype="func", desc="confounds", extension=".json")
         confounds = index.get(sub=sub, suffix="timeseries", datatype="func", desc="confounds", extension=".tsv")
-        paths_to_zip.extend(list(tsnr_fmriprep or []) + [spec_file] + list(confounds or []) + list(confounds_sidecar or []))
+        reports_folder = tmp_path / "reports" / f"sub-{sub}"
+        report_figures = [f for f in index.tags_by_paths.keys() if reports_folder in f.parents]
+        paths_to_zip.extend(list(tsnr_fmriprep or []) + [spec_file] + list(confounds or []) + list(confounds_sidecar or []) + (report_figures or []))
 
         # Create the zip file in the specified output directory
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
