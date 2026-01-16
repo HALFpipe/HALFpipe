19fe0a2ff1ab55c47751ce4032720e539306e17d
F-Tomas <fodran.tomas@gmail.com>
2026-01-16 08:34:53 +0100

    Update configurables.py

--- src/halfpipe/workflows/configurables.py
+++ src/halfpipe/workflows/configurables.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, field
 @dataclass
 class Configurables:
     reference_res: int = field(init=False)
+    reference_space: str = field(init=False)
 
 
 configurables = Configurables()
