50fbb8ff1e829fa62eef6963fcdf57a55ecab6a7
F-Tomas <fodran.tomas@gmail.com>
2026-01-16 08:44:32 +0100

    Update dual_regression.py

--- src/halfpipe/workflows/features/dual_regression.py
+++ src/halfpipe/workflows/features/dual_regression.py
@@ -161,7 +161,7 @@ def init_dualregression_wf(
         mem_gb=memcalc.series_std_gb,
     )
     if space == "standard":
-        resample.inputs.reference_space = Constants.reference_space
+        resample.inputs.reference_space = configurables.reference_space
         resample.inputs.reference_res = configurables.reference_res
     elif space == "native":
         workflow.connect(inputnode, "std2anat_xfm", resample, "transforms")
