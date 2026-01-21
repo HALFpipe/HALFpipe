ae2bcbc09666f892c8c23ff67be8279eabb33db3
F-Tomas <fodran.tomas@gmail.com>
2026-01-21 16:09:59 +0100

    consts to configs

--- tests/cli/commands/group_level/test_group_level.py
+++ tests/cli/commands/group_level/test_group_level.py
@@ -10,22 +10,32 @@ import nibabel as nib
 import numpy as np
 import pandas as pd
 import scipy
+from halfpipe.configurables import configurables
 from nilearn.image import new_img_like
 from templateflow.api import get as get_template
 
 from halfpipe.cli.commands.group_level.export import Statistic
 from halfpipe.cli.parser import parse_args
+from halfpipe.model.global_settings import GlobalSettingsSchema
 from halfpipe.resource import get as get_resource
 from halfpipe.stats.miscmaths import t2z_convert
-from halfpipe.workflows.constants import Constants
 
 from ....resource import setup as setup_test_resources
 
 
 def test_group_level(tmp_path: Path) -> None:
+    # since reference resolution and space are now configurables
+    # we need to first initial the globalsettingschema to get out
+    # the default values
+    schema = GlobalSettingsSchema()
+    global_settings = schema.dump({})
+
+    configurables.reference_res = global_settings["reference_res"]
+    configurables.reference_space = global_settings["reference_space"]
+
     template_query: dict[str, Any] = dict(
-        template=Constants.reference_space,
-        resolution=Constants.reference_res,
+        template=configurables.reference_space,
+        resolution=configurables.reference_res,
         desc="brain",
     )
     template_path = get_template(**template_query, suffix="T1w")
