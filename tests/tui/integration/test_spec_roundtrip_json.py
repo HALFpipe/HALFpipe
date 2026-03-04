8bf31973c9b993279564d757835bbdfe7d9742b3
F-Tomas <fodran.tomas@gmail.com>
2026-03-04 16:36:41 +0100

    need to run app manualy when without snap compare

--- tests/tui/integration/test_spec_roundtrip_json.py
+++ tests/tui/integration/test_spec_roundtrip_json.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import pytest
 from compare_jsons import compare_json_files
-from textual import pilot
 
 from tests.tui.spec_file_load_flow import run_before
 
@@ -10,15 +9,18 @@ from tests.tui.spec_file_load_flow import run_before
 @pytest.mark.asyncio
 async def test_load_from_spec_file_resave_spec_file(
     start_app, spec_file_dir_path: Path, downloaded_data_path: Path, covariant_spreadsheet_path: Path
-) -> None:
-    await run_before(
-        pilot,
-        data_path=downloaded_data_path,
-        spec_file_dir_path=spec_file_dir_path,
-        covariant_spreadsheet_path=covariant_spreadsheet_path,
-        feature_label=None,
-        scroll_to_remaining_part=False,
-    )
+):
+    app = start_app()
+
+    async with app.run_test() as pilot:
+        await run_before(
+            pilot,
+            data_path=downloaded_data_path,
+            spec_file_dir_path=spec_file_dir_path,
+            covariant_spreadsheet_path=covariant_spreadsheet_path,
+            feature_label=None,
+            scroll_to_remaining_part=False,
+        )
 
     file1 = spec_file_dir_path / "spec.json"
     file2 = spec_file_dir_path / "spec_reference.json"
